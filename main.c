ad4f159502df8ae6e9a9f92c238d07f48d371953
EGMJ <gabrieledson730@gmail.com>
2023-09-04 21:46:45 -0300

    problema fgest e ler musica resol

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 // criando uma estrutura de dados hoterogenea por enquanto
 struct Musica{
+    // char *nome;
     char nome[50];
     // int duracao;
     struct Musica *proxima;
@@ -19,10 +20,10 @@ struct Musica{
 // };
 
 void Inicializar(struct Musica **musica);
-void InserirMusica(struct Musica **musica, char *nome);
+void inserirMusica(struct Musica **musica, char *nome);
 int contarMusicas(struct Musica **musica, int cont);
-void ListarMusicas(struct Musica **musica);
-
+void listarMusicas(struct Musica **musica);
+void lendoMusica(char **nomeMusica);
 
 void main(void){
     setlocale(LC_ALL, "Portuguese");
@@ -30,14 +31,26 @@ void main(void){
     struct Musica *musica;
     Inicializar(&musica);
 
+    char *nomeAuxiliar;
+
+    lendoMusica(&nomeAuxiliar);
+
+    // strcpy((musica)->nome, nomeAuxiliar); // atribuindo o valor do char para o nome da estrutura. 
+    inserirMusica(&musica, nomeAuxiliar); // envio manual de musicas
+
     // adicionando uma musica no inicio
-    InserirMusica(&musica, "teste\n\n"); // envio manual de musicas
-    InserirMusica(&musica, "teste2\n\n"); // envio manual de musicas
-    InserirMusica(&musica, "teste"); // envio manual de musicas
-    printf("%d \n",contarMusicas(&musica,0)); // conta e imprime a quantidade de musicas
-    // printf("%s", &musica->nome);
-    ListarMusicas(&musica);
+    // inserirMusica(&musica, "teste1\n"); // envio manual de musicas
+    // inserirMusica(&musica, "teste2\n"); // envio manual de musicas
+    // inserirMusica(&musica, "teste3"); // envio manual de musicas
     
+    // inserirMusica(&musica, musica->nome); // envio manual de musicas
+
+    // printf("teste de imprecao %s", nomeTeste);
+
+    // inserirMusica(&musica, nome   Teste); // envio manual de musicas
+    printf("A quantidade de musicas na lista: %d\n",contarMusicas(&musica,0)); // conta e imprime a quantidade de musicas
+ 
+    listarMusicas(&musica);   
 }
 
 // Inicializando a lista de musicas
@@ -47,13 +60,13 @@ void Inicializar(struct Musica **musica){
 }
 
 // Inserindo musicas na lista
-void InserirMusica(struct Musica **musica, char *nome){
+void inserirMusica(struct Musica **musica, char *nome){
     if(*musica == NULL){// verificar se o primeiros valor esta vazio
         *musica = (struct Musica *) malloc(sizeof(struct Musica)); // alocando dinamicamente baseado no tamanha da estrutura.
         strcpy((*musica)->nome, nome); // atribuindo o valor do char para o nome da estrutura. 
         (*musica)->proxima = NULL; // definindo o proximo item da lista como nulo
     }else{ // se nao estiver nulo aplicar a recursividade para tratar o proximo como inicio
-        InserirMusica(&((*musica)->proxima), nome);
+        inserirMusica(&((*musica)->proxima), nome);
     }
 }
 
@@ -65,11 +78,11 @@ int contarMusicas(struct Musica **musica, int cont){
     }
 }
 
-void ListarMusicas(struct Musica **musica){
+void listarMusicas(struct Musica **musica){
     if((*musica) !=NULL){
         printf("%s, ",(*musica)->nome);
         if((*musica)->proxima !=NULL){
-            ListarMusicas(&((*musica)->proxima));
+            listarMusicas(&((*musica)->proxima));
         }
     }else{
         printf("A lista está vazia...");
@@ -79,15 +92,34 @@ void ListarMusicas(struct Musica **musica){
 
 /* TODO
     arrumar os warning de formato no char nome[50] alterar para dinamico
-    funcao listar musicas
-    funcao para remover musicas de duas formas, por nome e por ordem
+
+        1. Alocação de memória dinâmica para as músicas inseridas
+        
+        2. Uma função de busca de músicas por nomes
+        
+        4. Remoção de músicas por nome ou por ordem        
+
+        5. Adição de músicas sem ser manualmente
+      
+        6. A última música aponta para a primeira música da lista.
 */
 
 
-void buscarMusica(){}
+void lendoMusica(char **nomeMusica){
+    char auxNome[200];
 
+    /**
+     * procedimento para escolher a musica um switch case com uma lista de varias musicas
+     * e dependendo do valor selecionado atribui ao nome da musica
+    */
+
+    printf("Inserir nome da musica: \n");
+    fgets(auxNome,200,stdin);
+    *nomeMusica = (char *) malloc(strlen(auxNome)*sizeof(char));
+    strcpy(*nomeMusica, auxNome);
+}
+
+void buscarMusica(){}
 void removeMusica(){}
 void modifMusica(){}
-
-void lendoMusica(){}
 
